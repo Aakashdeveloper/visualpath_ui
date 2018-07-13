0626907bc5f84c4c0d6c257abf6cf8b4bf806baf
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-07-13 09:00:40 +0530

    A

--- NodeAPI/api.js
+++ NodeAPI/api.js
@@ -42,6 +42,36 @@ app.post('/add_user', (req,res)=>{
     })
 })
 
+app.put('/update_user', (req, res) => {
+	console.log('i was called, and i am updating the db');
+	console.log('Data received to update'+JSON.stringify(req.body));
+  console.log('_id is '+req.body._id);
+  db.collection(collection_name)
+  .findOneAndUpdate({"_id":req.body._id}, {
+    $set: {
+      last_name: req.body.last_name,
+      first_name: req.body.first_name,
+      email: req.body.email
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/delete_user', (req, res) => {
+	console.log('i was called, and i am deleting entry from the db');
+  console.log('Data i got is '+JSON.stringify(req.body));
+  db.collection(collection_name).findOneAndDelete({first_name: req.body.id},
+  (err, result) => {
+    if (err) return res.send(500, err)
+    res.send({message: 'success'})
+  })
+})
+
 MongoClient.connect(url, (err,client)=>{
     if(err) return console.log(err)
     db = client.db('carsApp')
